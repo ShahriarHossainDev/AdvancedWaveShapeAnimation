fd292449fdc9f3df0248f9ca060e3f348e533b6d
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-11-17 22:32:10 +0600

    Initial Commit

---
+++ AdvancedWaveShapeAnimation/AdvancedWaveShapeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedWaveShapeAnimationApp.swift
+//  AdvancedWaveShapeAnimation
+//
+//  Created by Shishir_Mac on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedWaveShapeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
